558a543823d4dfa12d11d471ffdd620191f42335
Rakesh <Rakesh@rakesh-pc>
2016-09-03 16:20:57 +0530

    adding queries

--- JDBCApp/src/main/resources/queries.sql
+++ JDBCApp/src/main/resources/queries.sql
@@ -1,4 +1,10 @@
-﻿create or replace function sum(IN n1 numeric, IN n2 numeric, OUT result numeric)
+﻿create table demo.emp_photo(
+id int not null,
+photo bytea not null,
+primary key(id)
+);
+
+create or replace function sum(IN n1 numeric, IN n2 numeric, OUT result numeric)
 AS $$
 BEGIN
 	result := n1 + n2;
